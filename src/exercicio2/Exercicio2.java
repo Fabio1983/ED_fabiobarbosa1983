316821cd01fa3217ef8fcf825f49a9b95e6b5f3b
Aluno <Aluno@LAB2PC6>
2014-09-01 19:22:32 -0300

    Exercicio 2

---
+++ src/exercicio2/Exercicio2.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exercicio2;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Fabio
+ */
+public class Exercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String[] pessoas = new String[50];
+       int nElems,i;
+       String qtd = null;
+            
+       qtd = JOptionPane.showInputDialog("Digite quantidade de pessoas: ");
+       nElems = Integer.parseInt(qtd);
+       
+       //---------------------------------------------
+       //-----------Insere----------------------------
+       //---------------------------------------------
+       
+       for(i=0;i<nElems;i++){
+       pessoas[i] = JOptionPane.showInputDialog("Digite nome, idade, profissao de pessoa [" + i + "]: ");
+       }
+       
+       //---------------------------------------------
+       //-----------Exibe----------------------------
+       //---------------------------------------------
+       
+       for(i=0;i<nElems;i++)
+        System.out.println(pessoas[i]);
+       
+       
+       
+       
+    
+    }
+}
---
+++ src/exercicio2/Pessoa.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exercicio2;
+
+/**
+ *
+ * @author Fabio
+ */
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String profissao;
+    
+    public Pessoa(String nome,int idade,String profissao){
+        this.nome = nome;
+        this.idade = idade;
+        this.profissao = profissao;
+    }
+    public void Insert(String nome,int idade,String profissao){
+        
+    }
+
+    
+    
+}
