ca4ee613ca046e7168139a91e1229fe5b061c9d7
Fabio <Fabio@Fabio-PC>
2014-09-13 15:13:18 -0300

    exerc3

---
+++ src/e3/Animais.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package e3;
+
+/**
+ *
+ * @author Fabio
+ */
+public class Animais {
+    private String cor;
+    private float tamanho;
+    
+    
+    
+}
