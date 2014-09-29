5b0c30ccf8a74c7889050b43b44e3a53f48bbe8b
Aluno <Aluno@LAB2PC6>
2014-09-29 20:40:32 -0300

    e6

---
+++ src/e6/Paciente.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package e6;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Paciente {
+    private String nome;
+    private String prioridade;
+    
+    public Paciente(String nome, String prioridade){
+        this.nome = nome;
+        this.prioridade = prioridade;
+    }
+    public void Cadastro(String nome,String prioridade){
+        
+    }
+    
+    
+    
+}
