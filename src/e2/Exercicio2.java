f0a9a59f8ebdd7a5353d15de7ae637ffa1e3d7a9
Aluno <Aluno@LAB2PC6>
2014-09-29 21:57:18 -0300

    e2

--- src/e2/Exercicio2.java
+++ src/e2/Exercicio2.java
@@ -7,7 +7,7 @@
 package e2;
 
 import java.util.Scanner;
-import javax.swing.JOptionPane;
+
 
 /**
  *
@@ -19,31 +19,43 @@ public class Exercicio2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       String[] pessoas = new String[50];
-       int nElems,i;
-       String qtd = null;
-            
-       qtd = JOptionPane.showInputDialog("Digite quantidade de pessoas: ");
-       nElems = Integer.parseInt(qtd);
-       
-       //---------------------------------------------
-       //-----------Insere----------------------------
-       //---------------------------------------------
-       
-       for(i=0;i<nElems;i++){
-       pessoas[i] = JOptionPane.showInputDialog("Digite nome, idade, profissao de pessoa [" + i + "]: ");
-       }
-       
-       //---------------------------------------------
-       //-----------Exibe----------------------------
-       //---------------------------------------------
-       
-       for(i=0;i<nElems;i++)
-        System.out.println(pessoas[i]);
-       
-       
-       
-       
-    
-    }
+       int maxSize = 100;            // coloca em uma constante o tamanho do vetor
+              Estrutura arr = new Estrutura(maxSize); // instancia a estrutura de dados
+              Scanner entrada = new Scanner (System.in); //Instancio o objeto entrada do tipo Scanner
+              Scanner entradaint = new Scanner (System.in);
+              System.out.println("Bem Vindo ao Programa de Vetores!!!");
+              
+              for (int i = 1; i < 6; i++) {
+                 System.out.println("Digite o nome " + i);
+                 String nome = entrada.nextLine();
+                 System.out.println("Digite a profissao do(a) " + nome);
+                 String profissao = entrada.nextLine();
+                 System.out.println("Digite a idade do(a) " + nome);
+                 int idade = entradaint.nextInt();
+                 arr.insert(nome, idade, profissao);
+              }
+
+              System.out.println("Muito Bem! Agora que você cadastrou os nomes, digite uma letra");
+
+              if (!arr.findAd(entrada.nextLine()))
+                  System.out.println("Não encontrado nenhum nome!!");
+              
+              System.out.println("Digite um nome para buscar:");
+              
+              int posicao = arr.findByName(entrada.nextLine());
+              
+              if (posicao != arr.size())
+                    System.out.println("Encontrei na posicao" + posicao );
+              else
+                    System.out.println("Não encontrado");
+              
+              System.out.println("Digite um nome ser removido:");
+              
+              if (arr.delete(entrada.nextLine()))   
+                    System.out.println("Removido!!!");
+              else  
+                   System.out.println("Não encontrado!!!");  
+              
+              arr.display();
+          }
 }
