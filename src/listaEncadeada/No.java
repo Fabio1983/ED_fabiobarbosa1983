7952aff6bc0892f728a06e52ee37f42a09d6078a
Aluno <Aluno@LAB2PC17>
2014-11-03 21:02:09 -0200

    e

--- src/listaEncadeada/No.java
+++ src/listaEncadeada/No.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package listaEncadeada;
 
-/**
- *
- * @author aleaguado traduzido de Robert Lafore
- */
+
 class No
    {
    private int iData;              // vai armazenar um valor numerico qualquer
@@ -18,7 +10,7 @@ class No
 // -------------------------------------------------------------
    public No(int id, double dd) // metodo construtor
       {
-      iData = id;                 // initializa os dois atributos do No
+      iData = id;                 // inicializa os dois atributos do No
       dData = dd;                 // o No next a principio fica como null
       }                           // 
 // -------------------------------------------------------------
